1cd1091a82c9d3f3c3c0f5514624bb1a853ec9d2
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-02-25 15:53:57 -0600

    first commit

---
+++ FizzBuzz.js
@@ -0,0 +1,19 @@
+
+function fizzBuzz(){
+    for(var i=1; i <= 100; i++){
+        if (i % 3 == 0 && i % 5 ==0) {
+            console.log(`FizzBuzz (${i})`)
+        }
+        if (i % 3 == 0){
+            console.log(`Fizz (${i})`)
+        }
+        if (i % 5 ==0){
+            console.log(`Buzz (${i})`)
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+
+fizzBuzz();
