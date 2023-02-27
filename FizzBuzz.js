b86cf196a75302f0fc3d80bac91c5f21eb11fc57
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-02-27 10:37:07 -0600

    restructured function for cleaner returns

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -4,10 +4,10 @@ function fizzBuzz(){
         if (i % 3 == 0 && i % 5 ==0) {
             console.log(`FizzBuzz (${i})`)
         }
-        if (i % 3 == 0){
+        else if (i % 3 == 0){
             console.log(`Fizz (${i})`)
         }
-        if (i % 5 ==0){
+        else if (i % 5 ==0){
             console.log(`Buzz (${i})`)
         }
         else {
